26f132307f4c3915630de65785c7f8f1f9dd7879
joao-hideki <joaoohideki12345@gmail.com>
2023-04-23 14:21:49 -0300

    add PokeSearchComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
 
 @NgModule({
-  declarations: [PokeHeaderComponent],
-  exports: [PokeHeaderComponent],
+  declarations: [PokeHeaderComponent, PokeSearchComponent],
+  exports: [PokeHeaderComponent, PokeSearchComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
