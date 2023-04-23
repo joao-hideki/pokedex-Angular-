ee45dd3790c9655586b7047c7b01e66e271bbfe2
joao-hideki <joaoohideki12345@gmail.com>
2023-04-23 20:00:41 -0300

    PokeApiService

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+
+  constructor(private http: HttpClient) {}
+
+  get apiLisAllPokemons(): Observable<any> {
+    return this.http.get<any>(this.url).pipe(
+      tap((res) => res),
+      tap((res) => {
+        res.results.map((resPokemon: any) => {
+          this.apiGetPokemons(resPokemon.url).subscribe(
+            (res) => (resPokemon.status = res)
+          );
+        });
+      })
+    );
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
+  }
+}
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
-  styleUrls: ['./poke-list.component.scss']
+  styleUrls: ['./poke-list.component.scss'],
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
+  constructor(private pokeApiService: PokeApiService) {}
 
+  ngOnInit(): void {
+    this.pokeApiService.apiLisAllPokemons.subscribe((res) => console.log(res));
+  }
 }
